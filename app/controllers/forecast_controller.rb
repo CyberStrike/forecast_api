bc94f50d07e08c372e476aa0fd9700577f32844b
jordanmarcr <jordanmarcr@gmail.com>
2014-06-12 19:41:38 -0400

    formatting for body and navbar

---
+++ app/controllers/forecast_controller.rb
@@ -0,0 +1,31 @@
+class ForecastController < ApplicationController
+
+	require 'forecast_io'
+	require 'typhoeus/adapters/faraday'
+	require 'geocoder'
+
+	def index
+
+		# city = request.location	
+		userSearch = params[:q]
+
+		if userSearch.blank?
+	 		# the geocoder is searching for city
+	 		@geocoder = request.location
+	 	else
+	 		@geocoder = Geocoder.search(userSearch).first
+	 	end
+
+	 	unless @geocoder.blank?
+
+	 		lat = @geocoder.latitude # lat = geocoder[0].geometry["location"]["lat"]
+
+	 		lng = @geocoder.longitude # lng = geocoder[0].geometry["location"]["lng"]
+
+	 		ForecastIO.api_key = '545bc8ee1ee9907df971316c7968014a'
+
+	 		@forecast = ForecastIO.forecast(lat, lng)
+	 	end
+	 end
+
+	end
